253902eb6adc5fe191843e63c92527da07f0afcd
Tiago <ti.almeida58@gmail.com>
2024-02-28 13:25:07 -0300

    feat: rest client spring boot

---
+++ src/test/java/br/com/demorestclient/DemoRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demorestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
